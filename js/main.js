08ba6aae61e21106df73f4426507a08985db4a7a
Alexander Cherba <alexander.cherba@gmail.com>
2018-03-29 17:58:31 +0300

    continue post form

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
-var message = 'Hello World!';
+function toHidePost() {
+    var post;
+    post = document.getElementById('postId');
 
-function addMessage(message) {
-    document.write(message);
+    if (post.hasAttribute('hidden')) {
+        console.log('was hidden');
+        post.removeAttribute('hidden');
+    }
+    else {
+        console.log('was not hidden');
+        post.setAttribute('hidden','hidden');
+    }
+    console.log('element: ', post);
 }
 
-addMessage(message);
+function toAddPost() {
+    let defaultPost = document.getElementById('postId');
+    console.log('new post: ', defaultPost);
+
+    let newPost = defaultPost.cloneNode(true);
+    newPost.setAttribute('id','post_01');
+    console.log('new post: ', newPost);
+    let parent = defaultPost.parentNode;
+    parent.appendChild(newPost);
+    //newPost.insertBefore(defaultPost);
+}
+
